0e68a97b8863713d4ad03b3b6ada10a833b91a64
ogeeDeveloper <omarogrant202@hotmail.com>
2022-01-15 10:17:46 -0500

    Added the useEffect to send http request and checks for chages in the cart store

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { useSelector } from 'react-redux'
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
@@ -6,6 +7,15 @@ import Products from './components/Shop/Products';
 
 const App = ()=>{
   const cartVisability = useSelector(state=>state.toggle.isToggled)
+
+  // Get state of overall cart
+  const cart = useSelector(state=>state.cart)
+
+  // Use useEffect to watch for chages in our cart, the useEffect will re-execute when the cart changes
+  useEffect(()=>{
+    // Sends http request to firebase database
+    fetch('https://fir-react-tutorial-2b75e-default-rtdb.firebaseio.com/cart.json',{method: "PUT", body: JSON.stringify(cart)})
+  },[cart])
   return (
     <Layout>
       {cartVisability && <Cart />}
